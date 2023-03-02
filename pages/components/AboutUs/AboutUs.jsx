455cfa87fd9d2386d2248cfa06862b5afbc32356
pm-moyanor <104218745+pm-moyanor@users.noreply.github.com>
2023-03-01 22:28:45 -0500

    add menu elements with unresolved CSS

--- pages/components/AboutUs/AboutUs.jsx
+++ pages/components/AboutUs/AboutUs.jsx
@@ -15,7 +15,7 @@ export default function AboutUs (){
       <h1 className="headtext__cormorant">About us</h1>
       <img src="/images/spoon.png"/>
       <p className="p__opensans">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis pharetra adipiscing ultrices vulputate posuere tristique. In sed odio nec aliquet eu proin mauris et.</p>
-      <button type="button" className="custom__button">Know more</button>
+      <button type="button" className="custom__button" onClick={()=>{}}>Know more</button>
     </div>
       <div className={`${styles['app__aboutus-content_knife']} flex__center`}>
         <img src="/images/knife.png"/>
@@ -24,7 +24,7 @@ export default function AboutUs (){
       <h1 className="headtext__cormorant">Our History</h1>
       <img src="/images/spoon.png"/>
       <p className="p__opensans">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis pharetra adipiscing ultrices vulputate posuere tristique. In sed odio nec aliquet eu proin mauris et.</p>
-      <button type="button" className="custom__button">Know more</button>
+      <button type="button" className="custom__button" onClick={()=>{}}>Know more</button>
     </div>
   </div>
   </div>
--- pages/components/SpecialMenu/SpecialMenu.jsx
+++ pages/components/SpecialMenu/SpecialMenu.jsx
@@ -1,11 +1,56 @@
-import React from 'react';
+
+import Subheading from "../Subheading/Subheading";
+import data from "../../../public/data";
+import styles from "./SpecialMenu.module.css";
+import MenuItem from "../MenuItem/MenuItem";
 
 
+const SpecialMenu = () => {
 
-const SpecialMenu = () => (
-  <div>
-    SpecialMenu
-  </div>
-);
+  console.log(data.wines)
+  return (
+    <div className={`${styles["app__specialMenu"]} flex__center section__padding`}>
+      <div className={`${styles["app__specialMenu-title"]}`}>
+        <img src="/images/spoon.png" />
+        <Subheading title={"Menu that fits your palatte"} />
+        <h1 className="headtext__cormornant"></h1>
+      </div>
+
+      <div className={`${styles["app__specialMenu-menu"]}`}>
+        <div className={`${styles["app__specialMenu-menu_wine"]} flex__center`}>
+          <p className={`${styles["app__specialMenu-menu_heading"]}`}>Wine & Beer</p>
+          <div className={`${styles["app__specialMenu-menu_items"]}`}>
+            {data.wines.map((wine, index) => {
+             return(
+              <MenuItem title={wine.title} price={wine.price} tags={wine.tags}/>
+              
+             ) 
+              
+            })}
+          </div>
+        </div>
+        <div className={`${styles["app__specialMenu-menu_img"]}`} >
+          <img src="/images/menu.png" /> 
+        </div>
+
+        <div className={`${styles["app__specialMenu-menu_cocktails"]} flex__center`}>
+          <p className={`${styles["app__specialMenu-menu_heading"]}`}>Cocktails</p>
+          <div className={`${styles["app__specialMenu-menu_items"]}`}>
+            {data.cocktails.map((cocktail, index) => {
+             return(
+              <MenuItem title={cocktail.title} price={cocktail.price} tags={cocktail.tags}/>
+              
+             ) 
+              
+            })}
+          </div>
+        </div>
+      </div>
+      <div style={{marginTop : '15px'}}>
+        <button className="custom__button" ></button>
+      </div>
+    </div>
+  );
+};
 
 export default SpecialMenu;
